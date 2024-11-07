e9792fa687f41894828ec608aa1716d2c10a6074
pjothi92 <124214114+pjothi92@users.noreply.github.com>
2024-11-07 12:45:38 -0500

    Update linearRegression.py
    added new changes to linearRegression, can be conducted through scikit learn now.

--- src/linearRegression.py
+++ src/linearRegression.py
@@ -1,30 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from cleaning import load_data, clean_data
-from normalization import normalize_data  
-
-# Will change implementation based on logistic regression function in HW3, as of now its based on tutorials
-#This function will give us our slope and intercept
-def getCoefficient(x, y):
-    #we take the number of data points
-    n = np.size(x) 
-    #we take the number of data points
-    mx = np.mean(x)
-    my = np.mean(y)
-    SSxy = np.sum(x*y) - n*my*mx
-    SSxx = np.sum(x*x) - n*mx*mx
-    #We calculate the slope and intercept
-    b1 = SSxy/SSxx
-    b0 = my - b1*mx
-    return (b0, b1)
+from sklearn.linear_model import LinearRegression
 
 # This function will plot all of our data
-def plotModel(x, y, b, title, correlation):
+def plotModel(x, y, b, m, title, correlation):
     #plot the data as a scatterplot
     plt.scatter(x, y, color="m", marker="o", s=30)
     #will project the predicted line based on our data
-    y1 = b[0] + b[1] * x
+    y1 = m*x + b
     plt.plot(x, y1, color="g") 
     plt.xlabel('Time Elapsed (hours)')
     plt.ylabel('Power Load (MW)')
@@ -33,30 +17,26 @@ def plotModel(x, y, b, title, correlation):
     plt.text(0.95, 0.9, f'Correlation: {correlation:.2f}', fontsize=12, color='blue', ha='right', transform=plt.gca().transAxes)
     plt.show()
 
-#This function calculates the correlation coefficient
-def calculateCorrelation(x, y):
-    correlation_matrix = np.corrcoef(x, y)
-    return correlation_matrix[0, 1]
-
 #This function reads the data from the CSV file (can alter based on how data preprocessing works)
 def regression(csv, dependent):
-    data = load_data(csv)
-    data = clean_data(data)
-    numerical_cols = data.select_dtypes(include=['int64', 'float64']).columns
-    data = normalize_data(data, numerical_cols)
+    data = pd.read_csv(csv)
     # Converting data in csv to correct format
     data['Datetime'] = pd.to_datetime(data['Datetime'])
     data['Time'] = (data['Datetime'] - data['Datetime'].min()).dt.total_seconds() / 3600.0
-    #Removing unecessary rows
-    data = data[['Time', dependent]].apply(pd.to_numeric, errors='coerce').dropna()
+    #Removing unnecessary rows
+    data = data[['Time', dependent]].dropna()
     #x and y are arrays that will store our data
-    x = data['Time'].values
+    x = data[['Time']].values
     y = data[dependent].values
-    b = getCoefficient(x, y)
-    correlation = calculateCorrelation(x, y)
-    plotModel(x, y, b, dependent, correlation)
+    #building a plotting the model
+    rModel = LinearRegression()
+    rModel.fit(x, y)
+    correlation = np.corrcoef(data['Time'], y)[0, 1]
+    m = rModel.coef_[0]
+    b = rModel.intercept_
+    plotModel(x, y, b, m, dependent, correlation)
 
 #Make sure to change to correct path (location) of where the data is located on the computer if you try running it on your computer.
 #The following line shows how to call the function.
-regression('/Users/pranavjothi/Documents/linearRegression/AEP_hourly.csv', 'AEP_MW')
+regression('/Users/pranavjothi/Documents/linearRegression/normalized_AEP_hourly.csv', 'AEP_MW')
 
